650cf91ef7dbf1cf95af3d381d6a45e1128fc4d4
Ahmed Rangel <ahmedrangel@outlook.com>
2024-05-29 12:14:57 -0500

    chore: remove unused code

--- plugins/primevue.ts
+++
@@ -1,8 +0,0 @@
-
-import PrimeVue from "primevue/config";
-import Chart from "primevue/chart";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(PrimeVue);
-  nuxtApp.vueApp.component("Chart", Chart);
-});
